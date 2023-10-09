c5511a6b2155c1a05f8e532fb31bc22b81484d65
KrasenKochev <krasenkochev89@gmail.com>
2023-10-09 15:24:30 +0300

    commit CreateCommentTests cleaned-up, and introducing constants for error messages

--- TestAutomationFramework/src/test/java/api/social/networ/api/tests/CreateCommentTests.java
+++ TestAutomationFramework/src/test/java/api/social/networ/api/tests/CreateCommentTests.java
@@ -19,9 +19,9 @@ import static org.testng.Assert.*;
 
 public class CreateCommentTests extends BaseTestSetup {
 
-    public static final String ERROR_MESSAGE_COMMENT_ID = format("Incorrect comment ID. Expected %s", COMMENT_ID);
-    public static final String ERROR_MESSAGE_LIKED_SHOULD_BE_FALSE = "Expected status should be false for disliked comment";
-    public static final String ERROR_MESSAGE_LIKED_SHOULD_BE_TRUE = "Expected status should be true for liked comment";
+    private static final String ERROR_MESSAGE_COMMENT_ID = format("Incorrect comment ID. Expected %s", COMMENT_ID);
+    private static final String ERROR_MESSAGE_LIKED_SHOULD_BE_FALSE = "Expected status should be false for disliked comment";
+    private static final String ERROR_MESSAGE_LIKED_SHOULD_BE_TRUE = "Expected status should be true for liked comment";
     private static final String ERROR_MESSAGE_RESPONSE_CONTENT = format("Response body content does not match the expected. Expected %s", COMMENT_DESCRIPTION);
     private static final String ERROR_MESSAGE_STATUS_CODE = format("Incorrect status code. Expected %s.", SC_OK);
     private static final String ERROR_MESSAGE_RESPONSE_BODY_EMPTY = "Response body should be empty";
