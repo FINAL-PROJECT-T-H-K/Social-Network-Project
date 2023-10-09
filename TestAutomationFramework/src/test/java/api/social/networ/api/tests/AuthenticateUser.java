5379a98ebb0279486016764fddac674746d2cc6f
KrasenKochev <krasenkochev89@gmail.com>
2023-10-09 21:01:08 +0300

    add upgradeExpertiseProfile to AuthenticateUser . To-Do - move constants and finish assertions

--- TestAutomationFramework/src/test/java/api/social/networ/api/tests/AuthenticateUser.java
+++ TestAutomationFramework/src/test/java/api/social/networ/api/tests/AuthenticateUser.java
@@ -1,22 +1,38 @@
 package api.social.networ.api.tests;
 
 import api.base.BaseTestSetup;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Locale;
+
 import static apisocialnetwork.Constants.*;
 import static apisocialnetwork.Endpoints.*;
 import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
 import static java.util.Objects.isNull;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.testng.Assert.assertEquals;
 
 public class AuthenticateUser extends BaseTestSetup {
 
+    public static  String RANDOM_JOB_TITLE_FIRST;
+
+    public static  String RANDOM_JOB_TITLE;
+
+
     @BeforeTest
     public static void setupAuthentication() {
+
         if (isNull(USER_ID)) {
             RegistrationTest registerUser = new RegistrationTest();
             registerUser.registerUser_Successful();
@@ -26,6 +42,7 @@ public class AuthenticateUser extends BaseTestSetup {
     @Test
     public void _02_authenticateAndFetchCookies() {
         baseURI = BASE_URL + AUTHENTICATE_ENDPOINT;
+
 
         System.out.println("Using Username: " + USERNAME);
         System.out.println("Using Password: " + PASSWORD); 
@@ -50,6 +67,52 @@ public class AuthenticateUser extends BaseTestSetup {
 
 
         ///add upgrade user expertise profile request
+    }
+    @Test
+    public void _03_upgradeExpertiseProfile() {
+
+        baseURI = BASE_URL + "/api/users/auth/" + USER_ID + "/expertise";
+
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("en-GB"), new RandomService());
+
+        RANDOM_JOB_TITLE_FIRST = fakeValuesService.bothify("JobTitle?????");
+        RANDOM_JOB_TITLE = fakeValuesService.bothify("JobTitles?????");
+
+        String body = String.format("{  \"availability\": 100,  " +
+                        "\"category\": {" +
+                        "    \"id\": 120,    " +
+                        "\"name\": \"Hairdresser\"  }" +
+                        ",  " +
+                        "\"id\": %s,  " +
+                        "\"skill1\": \"%s\",  " +
+                        "\"skills\": [    " +
+                        "\"%s\"  ]}"
+                ,USER_ID, RANDOM_JOB_TITLE_FIRST,RANDOM_JOB_TITLE);
+
+
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .header("Accept","*/*")
+                .cookie("JSESSIONID", COOKIE_VALUE)
+                .body(body)
+                .when()
+                .log()
+                .all()
+                .post(baseURI);
+
+        int statusCode = response.getStatusCode();
+        String skill1Content = response.getBody().jsonPath().getJsonObject("skill1");
+        String skillsContent = response.getBody().jsonPath().getString("skills");
+//        String userId = response.getBody().jsonPath().getString("id");
+
+        assertEquals(statusCode, SC_OK, format("Incorrect status code. Expected %s.", SC_OK));
+//        assertEquals(skill1Content, RANDOM_JOB_TITLE_FIRST, format("Response body content does not match the expected. Expected %s", RANDOM_JOB_TITLE_FIRST));
+//        assertEquals(skillsContent, RANDOM_JOB_TITLE, format("Response body content does not match the expected. Expected %s", RANDOM_JOB_TITLE));
+//        assertEquals(userId, USER_ID, format("Response body content does not match the expected. Expected %s", RANDOM_JOB_TITLE));
+
+        System.out.println(response.getBody().asPrettyString());
+
     }
 
 }
