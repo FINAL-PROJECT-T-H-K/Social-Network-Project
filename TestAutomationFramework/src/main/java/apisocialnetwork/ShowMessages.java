176f01e7c379b5d30ce212bc9abfb2da9222b2de
KrasenKochev <krasenkochev89@gmail.com>
2023-10-10 22:02:41 +0300

    Renamed SkillsManipulateTest and restructured it. Extracted methods from tests and added them to the BaseTestSetup. Added ShowMessages Class. Also added a helper method and dependency in pom.xml

---
+++ TestAutomationFramework/src/main/java/apisocialnetwork/ShowMessages.java
@@ -0,0 +1,11 @@
+package apisocialnetwork;
+
+import static apisocialnetwork.Constants.*;
+
+public class ShowMessages {
+
+    public static final String SHOW_MESSAGE_EDITED_SKILL = String.format("Skills with name '%s' was successfully edited.%n", EDITED_SKILLS + UNIQUE_NAME);
+    public static final String SHOW_MESSAGE_DELETED_SKILL = String.format("Skill with name'%s' was successfully deleted",SKILL_ID);
+    public static final String SHOW_MESSAGE_RESPONSE_BODY = "Show the response body: \n";
+    public static final String SHOW_MESSAGE_CREATED_SKILL_SILL_ID = String.format("Skills with name '%s' and Id '%s' was successfully created.%n",SKILL_DESCRIPTION, SKILL_ID);
+}
