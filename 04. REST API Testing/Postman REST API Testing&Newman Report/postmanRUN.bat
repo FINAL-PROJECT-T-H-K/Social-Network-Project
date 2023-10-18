d1e85a04032d0da68522b109f7a4479551014243
KrasenKochev <krasenkochev89@gmail.com>
2023-10-19 01:44:08 +0300

    Adding JmeterPerformance files and formatting the existing folders

--- 04. REST API Testing/Postman REST API Testing&Newman Report/postmanRUN.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd "Postman REST API Testing&Newman Report"       <----- NAME OF THE FOLDER
-
-newman run "WEare Social Network API.postman_collection.json" -e "SocialNetworkEnvironment.postman_environment.json" --reporters cli,htmlextra
-
-echo Postman collection execution completed.
